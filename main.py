83bc7146cb202640dae4cfdc405f098465974f07
Kim0816 <ktg00818@gmail.com>
2026-07-13 14:43:26 +0900

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.api.route.user_routers import router as user_router
-from app.api.route.agent_routers import router as agent_router
-
-app = FastAPI()
-app.include_router(user_router)
-app.include_router(agent_router)
-
